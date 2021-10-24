9aa97e40d7c67bd56b4c07d35583eec7e0a49b70
Nat Nat <natstonecipher@gmail.com>
2021-10-24 19:31:59 +0000

    button, slight diablo theme

--- src/App.js
+++ src/App.js
@@ -90,14 +90,24 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
-          <p className="sub-text">Team up to protect the Metaverse!</p>
+          <p className="header gradient-text">⚔️ Meta Diablo Verse ⚔️</p>
+          <p className="sub-text">Team up to protect the Metaverse from Diablo!</p>
         </div>
         <div className="connect-wallet-container">
           <img
-            src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
-            alt="Monty Python Gif"
+            src="https://c.tenor.com/pjLsAVyJPwwAAAAd/diablo-ii-remaster-resurrected.gif"
+            alt="D2 Diablo gif"
           />
+                      {/*
+             * Button that we will use to trigger wallet connect
+             * Don't forget to add the onClick event to call your method!
+             */}
+            <button
+              className="cta-button connect-wallet-button"
+              onClick={connectWalletAction}
+            >
+              Connect Wallet To Get Started
+            </button>
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
